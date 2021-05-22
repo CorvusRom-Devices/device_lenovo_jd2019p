a1fb4daab07fdfd4b0e195a23e3d1ea284661b49
HsyHusker <misterhusker19@gmail.com>
2021-05-22 16:16:27 +0800

    jd2019: clean up some stuffs
    - also nuking vendorsetup.sh, not needed anymore

--- init/init_jd2019.cpp
+++ init/init_jd2019.cpp
@@ -45,6 +45,4 @@ void vendor_load_properties() {
     property_override("ro.system.build.fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
     property_override("ro.product.build.fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
     property_override("ro.oem_unlock_supported", "0");
-    property_override("debug.stagefright.omx_default_rank.sw-audio", " ");
-    property_override("debug.stagefright.omx_default_rank", " ");
 }
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-rm -rf hardware/qcom/sdm845
-rm -rf system/extras/libperfmgr
