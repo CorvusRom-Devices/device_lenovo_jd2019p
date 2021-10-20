50da8389cb2eca0e3df33067be17f48fe0b5cdf7
Giammarco Senatore <giammasena@gmail.com>
2021-10-20 11:14:21 +0200

    jd2019: Drop updatable APEX

--- device.mk
+++ device.mk
@@ -6,9 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/jd2019/jd2019-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
